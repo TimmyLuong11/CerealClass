74204bc09390c7edef527e1e3514638fc11caadd
Timothy Luong <Timothy.Luong@ou.edu>
2021-04-22 22:15:07 -0500

    Initial Code

---
+++ CerealClass/CerealClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CerealClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
