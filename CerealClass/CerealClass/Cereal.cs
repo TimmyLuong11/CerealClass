10f025638699975be27150d19af46b636c5a8e9c
Timothy Luong <Timothy.Luong@ou.edu>
2021-04-27 22:39:42 -0500

    Added Class comments

--- CerealClass/CerealClass/Cereal.cs
+++ CerealClass/CerealClass/Cereal.cs
@@ -6,11 +6,17 @@ namespace CerealClass
 {
     class Cereal
     {
+        /// <summary>
+        /// Declaring public variables with getters and setters
+        /// </summary>
         public string Manufacturer { get; set; }
         public string Name { get; set; }
         public double Calories { get; set; }
         public double Cups { get; set; }
 
+        /// <summary>
+        /// Default Constructor setting each value to zero or empty
+        /// </summary>
         public Cereal()
         {
             Manufacturer = string.Empty;
