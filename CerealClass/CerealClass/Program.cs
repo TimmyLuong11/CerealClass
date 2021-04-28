aecfd11567dc664522ab34d3189275da28fa483e
Timothy Luong <Timothy.Luong@ou.edu>
2021-04-27 22:37:25 -0500

    Finished Code

--- CerealClass/CerealClass/Program.cs
+++ CerealClass/CerealClass/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CerealClass
 {
@@ -6,7 +7,48 @@ namespace CerealClass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Declaring Variables
+            string[] lines = File.ReadAllLines("Cereal_Data.txt");
+
+            //Looping to find the cereal with cups greater than or equal 1
+            Console.WriteLine("Cereal with Calories with cups greater than or equal to 1:\n");
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split("|");
+                double cup = Convert.ToDouble(pieces[3]);
+                if (cup >= 1 )
+                {
+                    Cereal c = new Cereal()
+                    {
+                        Name = pieces[0],
+                        Manufacturer = pieces[1],
+                        Calories = Convert.ToDouble(pieces[2]),
+                        Cups = Convert.ToDouble(pieces[3]),
+                    };
+                    Console.WriteLine($"{c.Name} is made by {c.Manufacturer} with servings size of {c.Cups} with {c.Calories} calories.");
+                }
+            }
+
+            //Loop to find the cereal with calories less then or equal to 100
+            Console.WriteLine("\nCereal with calories less than or equal to 100:\n");
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split("|");
+                double calories = Convert.ToDouble(pieces[2]);
+                if (calories <= 100)
+                {
+                    Cereal c = new Cereal()
+                    {
+                        Name = pieces[0],
+                        Manufacturer = pieces[1],
+                        Calories = Convert.ToDouble(pieces[2]),
+                        Cups = Convert.ToDouble(pieces[3]),
+                    };
+                    Console.WriteLine($"{c.Name} is made by {c.Manufacturer} with {c.Calories} calories and a servings size of {c.Cups}.");
+                }
+            }
         }
     }
 }
